a0db0fff653381f85b3b8e682544f31c364a040b
SigureMo <sigure.qaq@gmail.com>
2021-07-16 00:44:50 +0800

    ✨ feat: add yolov4 weights

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Download weights for vanilla YOLOv3
 wget -c "https://pjreddie.com/media/files/yolov3.weights" --header "Referer: pjreddie.com"
-# # Download weights for tiny YOLOv3
+# Download weights for tiny YOLOv3
 wget -c "https://pjreddie.com/media/files/yolov3-tiny.weights" --header "Referer: pjreddie.com"
 # Download weights for backbone network
 wget -c "https://pjreddie.com/media/files/darknet53.conv.74" --header "Referer: pjreddie.com"
+
+# Download weights for YOLOv4
+wget -c "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights"
+# Download weights for tiny YOLOv4
+wget -c "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights"
+# Download weights for YOLOv4 weights
+wget -c "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.conv.137"
