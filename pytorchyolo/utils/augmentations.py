cdc080f6dd7306c844dcd7694eecb596815839da
SigureMo <sigure.qaq@gmail.com>
2021-07-18 16:46:14 +0800

    ✨ feat: update StrongAug

--- pytorchyolo/utils/augmentations.py
+++ pytorchyolo/utils/augmentations.py
@@ -17,9 +17,15 @@ class DefaultAug(ImgAug):
 class StrongAug(ImgAug):
     def __init__(self, ):
         self.augmentations = iaa.Sequential([
-            iaa.Sometimes(0.5, iaa.Dropout([0.0, 0.01])),
-            iaa.Sometimes(0.5, iaa.Cutout((0, 10), size=(0.1, 0.3))),
-            iaa.Sometimes(0.5, iaa.Sharpen((0.0, 0.1))),
+            iaa.Sometimes(0.5, iaa.OneOf([
+                iaa.Dropout([0.0, 0.01]),
+                iaa.Cutout((0, 10), size=(0.1, 0.3), cval=200)
+            ])),
+            iaa.Sometimes(0.5, iaa.OneOf([
+                iaa.Sharpen((0.0, 0.1)),
+                iaa.MedianBlur(k=(3, 5)),
+            ])),
+            iaa.Sometimes(0.5, iaa.Crop(percent=(0, 0.1))),
             iaa.Sometimes(0.5, iaa.Affine(rotate=(-10, 10), translate_percent=(-0.1, 0.1), scale=(0.8, 1.5))),
             iaa.Sometimes(0.3, iaa.Rot90((1, 3))),
             iaa.AddToBrightness((-60, 40)),
